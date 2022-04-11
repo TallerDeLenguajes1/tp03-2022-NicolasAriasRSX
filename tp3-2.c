738cb72d4619683dd36536b5bb06a092b76541e5
NicolasArias <ariasmec06@gmail.com>
2022-04-11 11:58:31 -0300

    Subo punto tp3-2.c

---
+++ tp3-2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define TAM 5
+
+int main()
+{
+    char *v[TAM];
+    char *aux;
+    aux= (char *)malloc(50 * sizeof(char));//Reserva de memoria para una variable auxiliar
+    //Uso for para cargar cada uno de los elementos del vector.
+    printf("Carga de nombres en el vector.\n");
+    for (int i = 0; i < TAM; i++)
+    {
+        printf("%d) ingrese un nombre -> ", i+1);
+        gets(aux);
+        v[i]= (char *) malloc(strlen(aux) * 1 * sizeof(char));
+        strcpy(v[i], aux);
+    }
+    printf("Mostrando por pantalla los elementos del vector.\n");
+    for (int i = 0; i < TAM; i++)
+    {
+        puts(v[i]);
+        printf("\n");
+    }
+    //Limpiar la memoria ocupada.
+    for (int i = 0; i < TAM; i++)
+    {
+        free(v[i]);
+    }
+    free(aux);
+    
+    getchar();
+}
